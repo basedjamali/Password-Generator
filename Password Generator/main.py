f35455fae76220ca1b008a2e64d9b7afce4625fe
Based <basedjamali@gmail.com>
2023-02-26 21:52:46 +0100

    Updated main.py

--- Password Generator/main.py
+++ Password Generator/main.py
@@ -1,13 +1,17 @@
+import string
 import random
 
-upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-signs = ["^", "!", "§", "$", "%", "&", "/", "(", ")", "=", "?", "{", "[", "]", "}", "_", "-"]
+print("==== Password Generator ====")
 
-def generateRandomPassword():
-  passwordPart1 = random.choice(upperCaseLetters) + random.choice(lowerCaseLetters) + str(random.randint(0, 9)) + random.choice(signs) 
-  passwordPart2 = random.choice(upperCaseLetters) + random.choice(lowerCaseLetters) + str(random.randint(0, 9)) + random.choice(signs)
-  newPassword = passwordPart1 + passwordPart2
-  print('Your generated password is:', newPassword)
-  
-generateRandomPassword()
+letters = string.ascii_letters
+digits = string.digits
+characters = string.punctuation
+total = letters + digits + characters
+
+def generateRandomPassoword(): 
+  password = ""
+  length = int(input("Enter the length of the password: "))
+  for i in range(length):
+    password += "".join(random.choice(total))
+  print(password)
+generateRandomPassoword()
